74c629994842603cbdea0a88fb66c78c03a22e2c
Jasdeep <jasdeepajji@gmail.com>
2021-07-05 12:54:07 +0530

    view setup

--- src/views/history/List.js
+++ src/views/history/List.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 
 export default ({historyList, deleteRecord}) => {
     return historyList && historyList.map((row, i) =>
-    <ListGroup horizontal="sm" key={i} className="mt-2">
-      <ListGroupItem>{row.number}</ListGroupItem>
-      <ListGroupItem>{row.date}</ListGroupItem>
-      <ListGroupItem><Link to={`/print-sequence/${row.number}`}>Print view sequence</Link></ListGroupItem>
-      <ListGroupItem><Button color="danger" className="ml-2" onClick={()=>deleteRecord(i)}>Delete</Button></ListGroupItem>
-    </ListGroup>
+        <ListGroup horizontal="sm" key={i} className="mt-2">
+            <ListGroupItem>{row.number}</ListGroupItem>
+            <ListGroupItem>{row.date}</ListGroupItem>
+            <ListGroupItem><Link to={`/print-sequence/${row.number}`}>Print view sequence</Link></ListGroupItem>
+            <ListGroupItem><Button color="danger" className="ml-2" onClick={()=>deleteRecord(i)}>Delete</Button></ListGroupItem>
+        </ListGroup>
   )
 };
